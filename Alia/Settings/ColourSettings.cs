760be486a6d4aeda123a7041e4de391fb98fc7d6
i_Khanage <quiteimran@yahoo.co.uk>
2015-11-26 00:11:06 +0000

    Add colour settings

---
+++ Alia/Settings/ColourSettings.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Alia
+{
+	public static class ColourSettings
+	{
+		public static Color CompletedTask = Color.FromRgb(51, 153, 255);
+
+		public static Color TaskBackground = Color.FromRgb(102, 102, 153);
+
+		public static Color TaskEntryBackground = Color.FromRgb(112, 112, 163);
+
+		public static Color WhiteTextColour = Color.White;
+	}
+}
+
