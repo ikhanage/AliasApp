fadc5e59132ef3348b114e3195bbbe560f633f00
i_Khanage <quiteimran@yahoo.co.uk>
2015-12-07 20:55:37 +0000

    Base page class added

---
+++ Alia/Views/Tasks Page/TextTask.cs
@@ -0,0 +1,11 @@
+﻿namespace Alia
+{
+	public class TextTask 
+	{
+		public string Name { get; set; }
+		public string Text { get; set; }
+		public int UnlockCode { get; set; }
+		public PageTypes PageType { get; set; }
+		public bool Completed { get; set; }
+	}
+}
