0ba330ed931aa4dc6fa0aed6895ec63a6a6d03a0
i_Khanage <quiteimran@yahoo.co.uk>
2015-12-07 19:15:58 +0000

    Added task page

---
+++ Alia/Pages/TaskPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Alia
+{
+	public class TaskPage : ContentPage
+	{
+		public TaskPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
