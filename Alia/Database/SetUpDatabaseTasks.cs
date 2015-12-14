d5848a02108db89ed9c4a1124ffaa26917dcf44a
i_Khanage <quiteimran@yahoo.co.uk>
2015-12-14 21:06:59 +0000

    Files added

--- Alia/Database/SetUpDatabaseTasks.cs
+++ Alia/Database/SetUpDatabaseTasks.cs
@@ -8,10 +8,11 @@ namespace Alia
 		{
 			var textTasks = new List<TextTaskTable> ();
 
-			var taskTask = new TextTaskTable (1, TaskNames.TextTest, 000, 123);
+			var taskTask = new TextTaskTable (1, TaskNames.OneYear, 000, 101);
 			taskTask.Locked = false;
 
 			textTasks.Add (taskTask);
+			textTasks.Add (new TextTaskTable (2, TaskNames.TheHuntBegins, 101, 102));
 			return textTasks;
 		}
 
@@ -19,7 +20,7 @@ namespace Alia
 		{
 			var navTasks = new List<NavTaskTable> ();
 
-			navTasks.Add (new NavTaskTable(3, TaskNames.TextTest, 003, 004, 321));
+			//navTasks.Add (new NavTaskTable(3, TaskNames.TextTest, 003, 004, 321));
 			return navTasks;
 		}
 
@@ -27,9 +28,9 @@ namespace Alia
 		{
 			var quizTasks = new List<QuizTaskTable> ();
 
-			var quizTask = new QuizTaskTable (2, TaskNames.TextTest, 123, 003, "1", "2", "3", "4", "yeah", "Poop", "poo", "oop", 1);
+			//var quizTask = new QuizTaskTable (2, TaskNames.TextTest, 123, 003, "1", "2", "3", "4", "yeah", "Poop", "poo", "oop", 1);
 
-			quizTasks.Add (quizTask);
+			//quizTasks.Add (quizTask);
 			return quizTasks;
 		}
 	}
